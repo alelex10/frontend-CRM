df1f94cec8012c24405e6c7a473e0be7d97cdac6
alelex10 <elvizvida@gmail.com>
2025-10-06 18:50:50 -0300

    feat: Configure Storybook themes and add stories
    - Add `@storybook/addon-themes` dependency.
    - Import Roboto fonts in Storybook preview.
    - Configure Storybook to use light and dark themes with `ThemeProvider` and `CssBaseline`.
    - Add `globalTypes` and `initialGlobals` for theme selection in Storybook toolbar.
    - Create Storybook stories for `Page` and `NavBar` components.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,21 +1,62 @@
-import type { Preview } from '@storybook/nextjs-vite'
+import type { Preview } from "@storybook/nextjs-vite";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { lightThemeOKLCH} from "../src/app/themes/theme-light-mode";
+import { darkTheme } from "../src/app/themes/theme-dark-mode";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
 
-    a11y: {
-      // 'todo' - show a11y violations in the test UI only
-      // 'error' - fail CI on a11y violations
-      // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
-  },
+		a11y: {
+			// 'todo' - show a11y violations in the test UI only
+			// 'error' - fail CI on a11y violations
+			// 'off' - skip a11y checks entirely
+			test: "todo",
+		},
+	},
+	globalTypes: {
+		theme: {
+			description: "Global theme for components",
+			toolbar: {
+				// The label to show for this toolbar item
+				title: "Theme",
+				icon: "circlehollow",
+				// Array of plain string values or MenuItem shape (see below)
+				items: ["light", "dark"],
+				// Change title based on selected value
+				dynamicTitle: true,
+			},
+		},
+	},
+	initialGlobals: {
+		theme: "light",
+	},
 };
+
+
+
+export const decorators = [
+	withThemeFromJSXProvider({
+		themes: {
+			light: lightThemeOKLCH,
+			dark: darkTheme,
+		},
+		defaultTheme: "dark",
+		Provider: ThemeProvider,
+		GlobalStyles: CssBaseline,
+	}),
+];
 
 export default preview;
