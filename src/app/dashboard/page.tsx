73faa99fbabec4c1adfef3988341b99c878d0269
fwmc-dev <diegopaul483@gmail.com>
2025-10-28 20:32:36 +0000

    feat(dashboard): add MUI X for dashboard graphs and Dashboard view made (with mocked data)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,29 @@
-import AsideBar from "./components/aside-bar/aside-bar"
+//import { cookies } from "next/headers";
+//import type { request } from "next/server";
+import type { NextRequest } from "next/server";
+import DashboardClient from "./DashboardClient";
 
-const page = () => {
-  return (
-    <div>
+export default async function DashboardPage(request: NextRequest) {
+  // ✅ Leer cookie del request (lado servidor)
+  //const token = cookies().get("token")?.value;
+  /*const token = request.cookies.get("access_token");
 
-    </div>
-  )
+  if (!token) {
+    return <p>No estás autenticado</p>;
+  }
+
+  // ✅ Pedir datos al backend con ese token
+  const res = await fetch(`${process.env.API_URL}/dashboard`, {
+    headers: { Authorization: `Bearer ${token}` },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return <p>Error al cargar el dashboard</p>;
+  }
+
+  const data = await res.json();*/
+
+  return <DashboardClient /*data={data}*/ />;
 }
-export default page
+
